3260629bfdeab97357293d0fa9b26438da8eefd2
JeongEuna <inunwe02@khu.ac.kr>
2024-01-06 14:50:37 +0900

    Initial Commit

---
+++ 2480/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2480
+//
+//  Created by 정은아 on 2024/01/06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
